c7a5de970b7095eb3f1cafc58ddbc2a5243145f2
Petr Mohelník <mohelnik.petr@gmail.com>
2018-02-26 20:20:32 +0100

    vpn fix

--- settings/system.sh
+++ settings/system.sh
@@ -9,3 +9,7 @@
 
 # Add noacpi to kernel parameters on HP 530 laptop
 [[ $(sudo dmidecode | grep -c -i "HP 530")  -ne 0 ]] && [ $(grep -c -i "noacpi" /etc/default/grub ) -eq 0 ] && sudo sed -i -- 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="noacpi /' /etc/default/grub && sudo update-grub
+
+# Fix PPTP VPN in Ubuntu 17.10 with Gnome 3 (https://bbs.archlinux.org/viewtopic.php?id=153763)
+[[ $(lsb_release -rs) == "17.10" ]] && [[ ! -L /usr/lib/gnome-shell/nm-openconnect-auth-dialog ]] && sudo ln -s /usr/lib/networkmanager/nm-openconnect-auth-dialog /usr/lib/gnome-shell/
+
